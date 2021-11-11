da25ae6dc4dfc3bbeb331feee432a3c60b6702bb
Joseph Goolic <joegoolic15@gmail.com>
2021-11-10 22:42:12 -0500

    borders added and website icon

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head'
 import Image from 'next/image'
-import Link from "next/link"
+import Head from 'next/head';
 import Topbar from "../components/topbar/Topbar";
 import Intro from "../components/intro/Intro";
 import Skills from "../components/skills/skills";
@@ -8,34 +7,28 @@ import Portfolio from "../components/portfolio/Portfolio";
 import Contact from "../components/contact/Contact";
 import Menu from "../components/menu/Menu";
 import stylesapp from "./app.module.scss";
-import useSWR from 'swr';
+import JG from '../public/JG.svg'
 import { useState } from "react";
 import { API_URL } from "../config/index";
 
 function Home(projects) {
     const [menuOpen, setMenuOpen] = useState(false)
     const [animationOn, setAnimationOn] = useState(false)
-    //const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    //const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-    //console.log(vh, vw)
     return (
         <div>
-            <div>
+            <Head>
                 <meta charSet="utf-8" />
-                <link rel="icon" href="/JG.png" />
+                <link rel="icon" type = "image/svg" sizes = "32x32" href='/jg.svg' />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="theme-color" content="#000000" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap"
-                    rel="stylesheet"
-                />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap"rel="stylesheet"/>
                 <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
                 <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
                 <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&family=Space+Grotesk:wght@300;400;500;600;700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
                 <title>Joseph Goolic</title>
-            </div>
+            </Head>
             <div className={stylesapp.app}>
                 <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
                 <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
@@ -44,10 +37,11 @@ function Home(projects) {
                     <Skills animationOn={animationOn} setAnimationOn={setAnimationOn} />
                     <Portfolio projects={projects} />
                     <Contact />
-
                 </div>
             </div>
         </div>
+               
+       
     );
 }
 export async function getServerSideProps() {
